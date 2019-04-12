bb52fe5edab05e3eb3832db31afa2ca76daf38cf
SergeyArenzon <sergeyr1991@gmail.com>
2019-04-12 19:28:30 +0300

    deleted -bad input unit

--- PhysicalNumber.cpp
+++ PhysicalNumber.cpp
@@ -138,7 +138,7 @@ ariel::Unit ariel::strToUnit(string str){
     else if(str.compare("g")==0) return Unit::G;      
     else if(str.compare("kg")==0) return Unit::KG;      
     else if(str.compare("ton")==0) return Unit::TON;    
-    throw std::runtime_error("Bad input unit");
+    
 }
 
 bool ariel::ifDifUnit(int x,int y){
