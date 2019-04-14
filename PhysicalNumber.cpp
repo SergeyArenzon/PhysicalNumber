9d9f8c08d1469e7175de4dac7dfd6761f63875b8
SergeyArenzon <sergeyr1991@gmail.com>
2019-04-14 13:07:33 +0300

    fixed ++postfix

--- PhysicalNumber.cpp
+++ PhysicalNumber.cpp
@@ -135,7 +135,7 @@ ariel::Unit ariel::strToUnit(string str){
     else if(str.compare("g")==0) return Unit::G;      
     else if(str.compare("kg")==0) return Unit::KG;      
     else if(str.compare("ton")==0) return Unit::TON;    
-return Unit::CM;
+return Unit::G;
 }
 bool ariel::ifDifUnit(int x,int y){
     if(0<=x && x<=2 && 0<=y && y<=2) return true;
